d0a0d33f181e7d9d7b8a4265c4917da8b37a02f2
TechRami <techrami@outlook.com>
2023-02-04 18:40:19 +0300

    added cors

--- server.js
+++ server.js
@@ -1,55 +1,67 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const app = express();
 require('dotenv').config();
+const cors = require("cors");
+const app = express();
 
+app.use(cors({
+  origin: process.env.ORIGIN,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 
 app.use(express.json());
 
 app.post("/send-email", async (req, res) => {
-  const { email, subject, content } = req.body;
+  try {
+    const { email, subject, content } = req.body;
 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.ACC_EMAIL,
-      pass: process.env.ACC_PASS,
-    },
-  });
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.ACC_EMAIL,
+        pass: process.env.ACC_PASS,
+      },
+    });
 
-  let info = await transporter.sendMail({
-    from: email,
-    to: process.env.SENT_TO,
-    subject: subject,
-    html: `
-    <html>
-      <head>
-        <style>
-          a {
-            color: blue;
-          }
-          p {
-            font-size: 16px;
-          }
-          .subject {
-            font-size: 20px;
-            font-weight: bold;
-          }
-        </style>
-      </head>
-      <body>
-        <a>Email from ${email}</a>
-        <p class="subject">العنوان: ${subject}</p>
-        <p>الرسالة: ${content}</p>
-      </body>
-    </html>
-  `,
-  });
+    let info = await transporter.sendMail({
+      from: email,
+      to: process.env.SENT_TO,
+      subject: subject,
+      html: `
+      <html>
+        <head>
+          <style>
+            a {
+              color: blue;
+            }
+            p {
+              font-size: 16px;
+            }
+            .subject {
+              font-size: 20px;
+              font-weight: bold;
+            }
+          </style>
+        </head>
+        <body>
+          <a>Email from ${email}</a>
+          <p class="subject">العنوان: ${subject}</p>
+          <p>الرسالة: ${content}</p>
+        </body>
+      </html>
+    `,
+    });
 
-  res.status(200).json({ message: "Email sent successfully" });
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to send email" });
+  }
 });
 
 const port = process.env.PORT || 5000;
